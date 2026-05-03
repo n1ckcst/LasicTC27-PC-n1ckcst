72121e763a2962c3655b89d097b212ae98cf2899
n1ckcst <lasic.nickshanes@gmail.com>
2026-05-03 13:15:53 +0800

    base code

---
+++ PSA2_Technical/index.php
@@ -0,0 +1,268 @@
+
+<?php include'MyVariables.php'; ?>
+
+
+<!Doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name= "viewport" content="width=device-width, initial-scale=1.0">
+    <title>PSA2 Technical</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<hr>
+<table class = "tableheader">
+    <tr>
+        <th>Name</th>
+        <th>Date</th>
+    </tr>
+</table>
+
+<h1>MEASURE CONVERSION CHART -- Lengths (UK)</h1>
+
+
+<body>
+
+<table class = "conversions">
+  <thead>
+    <tr>
+        <th colspan="6" style="text-align: center; background-color: yellow; color: black;"> METRIC CONVERSIONS </th>
+    </tr>
+ </thead>
+    <tr>
+      <th>placeholder</th>
+    <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+      <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+    </tr>
+  
+  <tbody>
+    <tr>
+     <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+      <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+    </tr>
+
+     <tr>
+     <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+      <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+    </tr>
+
+     <tr>
+     <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+      <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+    </tr>
+  </tbody>
+</table>
+
+
+<table class = "metric_conversions">
+  <thead>
+    <tr>
+        <th colspan="6" style="text-align: center; background-color: yellow; color: black;"> IMPERIAL CONVERSIONS </th>
+    </tr>
+ </thead>
+    <tr>
+      <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+      <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+    </tr>
+  
+  <tbody>
+    <tr>
+     <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+      <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+    </tr>
+
+     <tr>
+     <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+      <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+    </tr>
+
+     <tr>
+     <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+      <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+    </tr>
+
+      <tr>
+     <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+      <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+    </tr>
+  </tbody>
+</table>
+
+
+<table class = "metric_conversions">
+  <thead>
+    <tr>
+        <th colspan="6" style="text-align: center; background-color: yellow; color: black;"> METRIC ⮕ IMPERIAL CONVERSIONS </th>
+    </tr>
+ </thead>
+    <tr>
+      <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+      <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+    </tr>
+  
+  <tbody>
+    <tr>
+     <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+      <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+    </tr>
+
+     <tr>
+     <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+      <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+    </tr>
+
+     <tr>
+     <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+      <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+    </tr>
+
+      <tr>
+     <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+      <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+    </tr>
+
+      <tr>
+     <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+      <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+    </tr>
+
+      <tr>
+     <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+      <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+    </tr>
+  </tbody>
+</table>
+
+<table class = "metric_conversions">
+  <thead>
+    <tr>
+        <th colspan="6" style="text-align: center; background-color: yellow; color: black;"> IMPERIAL ⮕ METRIC CONVERSIONS </th>
+    </tr>
+ </thead>
+    <tr>
+      <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+      <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+    </tr>
+  
+  <tbody>
+    <tr>
+     <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+      <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+    </tr>
+
+     <tr>
+     <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+      <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+    </tr>
+
+     <tr>
+     <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+      <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+    </tr>
+
+      <tr>
+     <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+      <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+    </tr>
+
+      <tr>
+     <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+      <th>placeholder</th>
+      <th style="text-align: center;">=</th>
+      <th>placeholder</th>
+    </tr>
+
+  </tbody>
+</table>
+
+</body>
+
+
+<footer class = "footer">
+    <p>Created By Nick Shane S Lasic 202410346</p> 
