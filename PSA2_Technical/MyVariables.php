35dd47cbe4ac0548b37d4ec4127a260b28ca537c
n1ckcst <lasic.nickshanes@gmail.com>
2026-05-03 14:14:53 +0800

    semi finished

--- PSA2_Technical/MyVariables.php
+++ PSA2_Technical/MyVariables.php
@@ -34,4 +34,30 @@ $yards_from_mile     = $mile_val * 1760;
 
 
 $furlongs_from_mile  = $mile_val * 8;
+
+
+// -----------------------------------------------------------------------
+$mm_val = 1;
+$cm_val = 1;
+$m_val  = 1;
+$km_val = 1;
+
+
+$inches_from_mm = $mm_val * 0.03937;
+$inches_from_cm = $cm_val * 0.39370;
+$inches_from_m  = $m_val  * 39.37008;
+$feet_from_m    = $m_val  * 3.28084;
+$yards_from_m   = $m_val  * 1.09361;
+$yards_from_km  = $km_val * 1093.6133;
+$miles_from_km  = $km_val * 0.62137;
+
+
+//------------------------------------------------------------------------
+
+$cm_from_1inch = 2.54;       
+$cm_from_1foot = 30.48;      
+$cm_from_1yard = 91.44;      
+$m_from_1yard = 0.9144;      
+$m_from_1mile = 1609.344;     
+$km_from_1mile = 1.609344;    
 ?>
--- PSA2_Technical/index.php
+++ PSA2_Technical/index.php
@@ -24,15 +24,15 @@
 
 <body>
 
-<table class = "conversions">
+<table class="conversions">
   <thead>
     <tr>
-        <th colspan="6" style="text-align: center; background-color: yellow; color: black;"> METRIC CONVERSIONS </th>
+        <th colspan="6" style="text-align: center; background-color: yellow; color: black;"><strong> METRIC CONVERSIONS </strong></th>
     </tr>
- </thead>
+  </thead>
     <tr>
       <th>1 Centimeter</th>
-    <th style="text-align: center;">=</th>
+      <th style="text-align: center;">=</th>
       <th><?php echo $millimetre; ?> Millimeters</th>
       <th>1 cm</th>
       <th style="text-align: center;">=</th>
@@ -41,7 +41,7 @@
   
   <tbody>
     <tr>
-     <th>1 Decimeter</th>
+      <th>1 Decimeter</th>
       <th style="text-align: center;">=</th>
       <th><?php echo $centimetre_from_dm; ?> Centimeters</th>
       <th>1 dm</th>
@@ -49,31 +49,30 @@
       <th><?php echo $centimetre_from_dm; ?> cm</th>
     </tr>
 
-     <tr>
-     <th>1 Meter</th>
-      <th style="text-align: center;">=</th>
-      <th><?php echo $centimetre_from_m; ?> Centimeters</th>
-      <th>1 m</th>
-      <th style="text-align: center;">=</th>
-      <th><?php echo $centimetre_from_m; ?> cm</th>
-    </tr>
+      <tr>
+        <th>1 Meter</th>
+        <th style="text-align: center;">=</th>
+        <th><?php echo $centimetre_from_m; ?> Centimeters</th>
+        <th>1 m</th>
+        <th style="text-align: center;">=</th>
+        <th><?php echo $centimetre_from_m; ?> cm</th>
+      </tr>
 
-     <tr>
-     <th>1 Kilometer</th>
-      <th style="text-align: center;">=</th>
-      <th><?php echo $metre_from_km; ?> Meters</th>
-      <th>1 km</th>
-      <th style="text-align: center;">=</th>
-      <th><?php echo $metre_from_km; ?> m</th>
-    </tr>
+      <tr>
+        <th>1 Kilometer</th>
+        <th style="text-align: center;">=</th>
+        <th><?php echo $metre_from_km; ?> Meters</th>
+        <th>1 km</th>
+        <th style="text-align: center;">=</th>
+        <th><?php echo $metre_from_km; ?> m</th>
+      </tr>
   </tbody>
-</table>
 
 
 <table class = "metric_conversions">
   <thead>
     <tr>
-        <th colspan="6" style="text-align: center; background-color: yellow; color: black;"> IMPERIAL CONVERSIONS </th>
+        <th colspan="6" style="text-align: center; background-color: yellow; color: black;"><strong> IMPERIAL CONVERSIONS </strong></th>
     </tr>
  </thead>
     <tr>
@@ -125,139 +124,134 @@
 </table>
 
 
-<table class = "metric_conversions">
+<table class="conversions" border="1" style="border-collapse: collapse; width: 100%;">
   <thead>
     <tr>
-        <th colspan="6" style="text-align: center; background-color: yellow; color: black;"> METRIC ⮕ IMPERIAL CONVERSIONS </th>
+      <th colspan="6" style="text-align: center; background-color: yellow; color: black; padding: 10px;">
+       <strong> METRIC ⮕ IMPERIAL CONVERSIONS</strong>
+      </th>
     </tr>
- </thead>
-    <tr>
-      <th>placeholder</th>
-      <th style="text-align: center;">=</th>
-      <th>placeholder</th>
-      <th>placeholder</th>
-      <th style="text-align: center;">=</th>
-      <th>placeholder</th>
-    </tr>
-  
+  </thead>
   <tbody>
     <tr>
-     <th>placeholder</th>
-      <th style="text-align: center;">=</th>
-      <th>placeholder</th>
-      <th>placeholder</th>
-      <th style="text-align: center;">=</th>
-      <th>placeholder</th>
+      <td>1 Millimeter</td>
+      <td style="text-align: center;">=</td>
+      <td><?php echo $inches_from_mm; ?> Inches</td>
+      <td>1 mm</td>
+      <td style="text-align: center;">=</td>
+      <td><?php echo $inches_from_mm; ?> in</td>
     </tr>
-
-     <tr>
-     <th>placeholder</th>
-      <th style="text-align: center;">=</th>
-      <th>placeholder</th>
-      <th>placeholder</th>
-      <th style="text-align: center;">=</th>
-      <th>placeholder</th>
+    <tr>
+      <td>1 Centimeter</td>
+      <td style="text-align: center;">=</td>
+      <td><?php echo $inches_from_cm; ?> Inches</td>
+      <td>1 cm</td>
+      <td style="text-align: center;">=</td>
+      <td><?php echo $inches_from_cm; ?> in</td>
     </tr>
-
-     <tr>
-     <th>placeholder</th>
-      <th style="text-align: center;">=</th>
-      <th>placeholder</th>
-      <th>placeholder</th>
-      <th style="text-align: center;">=</th>
-      <th>placeholder</th>
+    <tr>
+      <td>1 Meter</td>
+      <td style="text-align: center;">=</td>
+      <td><?php echo $inches_from_m; ?> Inches</td>
+      <td>1 m</td>
+      <td style="text-align: center;">=</td>
+      <td><?php echo $inches_from_m; ?> in</td>
     </tr>
-
-      <tr>
-     <th>placeholder</th>
-      <th style="text-align: center;">=</th>
-      <th>placeholder</th>
-      <th>placeholder</th>
-      <th style="text-align: center;">=</th>
-      <th>placeholder</th>
+    <tr>
+      <td>1 Meter</td>
+      <td style="text-align: center;">=</td>
+      <td><?php echo $feet_from_m; ?> Feet</td>
+      <td>1 m</td>
+      <td style="text-align: center;">=</td>
+      <td><?php echo $feet_from_m; ?> ft</td>
     </tr>
-
-      <tr>
-     <th>placeholder</th>
-      <th style="text-align: center;">=</th>
-      <th>placeholder</th>
-      <th>placeholder</th>
-      <th style="text-align: center;">=</th>
-      <th>placeholder</th>
+    <tr>
+      <td>1 Meter</td>
+      <td style="text-align: center;">=</td>
+      <td><?php echo $yards_from_m; ?> Yards</td>
+      <td>1 m</td>
+      <td style="text-align: center;">=</td>
+      <td><?php echo $yards_from_m; ?> yd</td>
     </tr>
-
-      <tr>
-     <th>placeholder</th>
-      <th style="text-align: center;">=</th>
-      <th>placeholder</th>
-      <th>placeholder</th>
-      <th style="text-align: center;">=</th>
-      <th>placeholder</th>
+    <tr>
+      <td>1 Kilometer</td>
+      <td style="text-align: center;">=</td>
+      <td><?php echo $yards_from_km; ?> Yards</td>
+      <td>1 km</td>
+      <td style="text-align: center;">=</td>
+      <td><?php echo $yards_from_km; ?> yd</td>
+    </tr>
+    <tr>
+      <td>1 Kilometer</td>
+      <td style="text-align: center;">=</td>
+      <td><?php echo $miles_from_km; ?> Miles</td>
+      <td>1 km</td>
+      <td style="text-align: center;">=</td>
+      <td><?php echo $miles_from_km; ?> mi</td>
     </tr>
   </tbody>
 </table>
 
-<table class = "metric_conversions">
+<table class="conversions">
   <thead>
     <tr>
-        <th colspan="6" style="text-align: center; background-color: yellow; color: black;"> IMPERIAL ⮕ METRIC CONVERSIONS </th>
+        <th colspan="6" style="text-align: center; background-color: yellow; color: black;"> <strong> IMPERIAL ⮕ METRIC CONVERSIONS </strong></th>
     </tr>
- </thead>
-    <tr>
-      <th>placeholder</th>
-      <th style="text-align: center;">=</th>
-      <th>placeholder</th>
-      <th>placeholder</th>
-      <th style="text-align: center;">=</th>
-      <th>placeholder</th>
-    </tr>
-  
+  </thead>
   <tbody>
     <tr>
-     <th>placeholder</th>
+      <th>1 Inch</th>
       <th style="text-align: center;">=</th>
-      <th>placeholder</th>
-      <th>placeholder</th>
+      <th><?php echo $cm_from_1inch; ?> Centimeters</th>
+      <th>1 in</th>
       <th style="text-align: center;">=</th>
-      <th>placeholder</th>
+      <th><?php echo $cm_from_1inch; ?> cm</th>
     </tr>
 
-     <tr>
-     <th>placeholder</th>
+    <tr>
+      <th>1 Foot</th>
       <th style="text-align: center;">=</th>
-      <th>placeholder</th>
-      <th>placeholder</th>
+      <th><?php echo $cm_from_1foot; ?> Centimeters</th>
+      <th>1 ft</th>
       <th style="text-align: center;">=</th>
-      <th>placeholder</th>
+      <th><?php echo $cm_from_1foot; ?> cm</th>
     </tr>
 
-     <tr>
-     <th>placeholder</th>
+    <tr>
+      <th>1 Yard</th>
       <th style="text-align: center;">=</th>
-      <th>placeholder</th>
-      <th>placeholder</th>
+      <th><?php echo $cm_from_1yard; ?> Centimeters</th>
+      <th>1 yd</th>
       <th style="text-align: center;">=</th>
-      <th>placeholder</th>
+      <th><?php echo $cm_from_1yard; ?> cm</th>
     </tr>
 
-      <tr>
-     <th>placeholder</th>
+    <tr>
+      <th>1 Yard</th>
       <th style="text-align: center;">=</th>
-      <th>placeholder</th>
-      <th>placeholder</th>
+      <th><?php echo $m_from_1yard; ?> Meters</th>
+      <th>1 yd</th>
       <th style="text-align: center;">=</th>
-      <th>placeholder</th>
+      <th><?php echo $m_from_1yard; ?> m</th>
     </tr>
 
-      <tr>
-     <th>placeholder</th>
+    <tr>
+      <th>1 Mile</th>
       <th style="text-align: center;">=</th>
-      <th>placeholder</th>
-      <th>placeholder</th>
+      <th><?php echo $m_from_1mile; ?> Meters</th>
+      <th>1 mi</th>
       <th style="text-align: center;">=</th>
-      <th>placeholder</th>
+      <th><?php echo $m_from_1mile; ?> m</th>
     </tr>
 
+    <tr>
+      <th>1 Mile</th>
+      <th style="text-align: center;">=</th>
+      <th><?php echo $km_from_1mile; ?> Kilometers</th>
+      <th>1 mi</th>
+      <th style="text-align: center;">=</th>
+      <th><?php echo $km_from_1mile; ?> km</th>
+    </tr>
   </tbody>
 </table>
 
