b96243dc49a57e2e0f742ab5f69cfb5a5b39af2c
n1ckcst <lasic.nickshanes@gmail.com>
2026-05-13 12:54:30 +0800

    base code

---
+++ PSA3_Main/Index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+    <title>PSA2 - Main Page | Nick Shane S Lasic</title>
+    <link rel="stylesheet" href="IndexStyle.css">
+</head>
+<body>
+
+<div class="main-card">
+    <h1>📁 Nick Shane S Lasic</h1>
+    <div class="subtitle">PRE-SUMMATIVE ASSESSMENT 3</div>
+    <div class="desc">Applications Development and Emerging Technologies</div>
+    
+    <div class="button-group">
+        <a href="Number1Act.php" class="project-btn">
+            <span>Using Arrays: Student Records</span>
+            <span class="btn-icon">➡️</span>
+        </a>
+        <a href="Number2Act.php" class="project-btn">
+            <span>Using Arrays: Number </span>
+            <span class="btn-icon">➡️</span>
+        </a>
+        <a href="Number3Act.php" class="project-btn">
+            <span>Using User-Defined Functions</span>
+            <span class="btn-icon">➡️</span>
+        </a>
+    </div>
+    
+    <div class="footer">
+        © 2026 Nick Shane S Lasic | 202410346
+    </div>
+</div>
+
+</body>
+</html>
